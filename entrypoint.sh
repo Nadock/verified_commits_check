7a81c1d5da40b933351fdb514f08133899e174b5
Riley Chase <riley.chase@rileychase.net>
2020-06-25 19:05:32 +0930

    Better shell set config to exit on error correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
-set +x
+# e: Exit immediately on fail
+# E: Inherit ERR trap so it works correctly if something fails and we exit because of -e
+# u: Treat unset variables as errors
+# x: Print each instruction to stderr before executing
+# o pipefail: Exit status of pipe is non-zero if any step in pipe fails
+set -Eeuxo pipefail
+
 echo "--- verified commits alert ---"
 env
 echo ""
