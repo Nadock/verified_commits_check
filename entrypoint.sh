c5038d9f1efdad4bfff19769b0155faf5de77518
Riley Chase <riley.chase@studylink.com>
2021-12-21 15:01:09 +1030

    Remove `-o pipefail` from entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 # e: Exit immediately on fail
 # u: Treat unset variables as errors
 # x: Print each instruction to stderr before executing
-# o pipefail: Exit status of pipe is non-zero if any step in pipe fails
-set -euxo pipefail
+set -eux
 
 cd /opt/action
 python3 -m src.action
