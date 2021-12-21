da6ac73f6c5499fba0610c7997280cdde57e0695
Riley Chase <riley.chase@studylink.com>
2021-12-21 15:04:00 +1030

    Add comment explaining why the entrypoint script is required

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+#
+# This entrypoint script is used to work around the way GitHub Actions runs Docker
+# based actions. Specifically, they set a custom WORKDIR which overrides the
+# container's configured WORKDIR.
+#
 # e: Exit immediately on fail
 # u: Treat unset variables as errors
 # x: Print each instruction to stderr before executing
